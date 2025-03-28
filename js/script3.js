e690faad846f7b29a327ffb07789d46f14903820
Unai Benavente <161081469+benas20@users.noreply.github.com>
2025-03-28 22:48:49 +0100

    Update script3.js

--- js/script3.js
+++ js/script3.js
@@ -1,16 +1,13 @@
-// Seleccionamos las imágenes que deben desaparecer y el contenedor slide
-const content = document.querySelectorAll('.content');
-content.forEach(item => {
-  const slideTriggerImage = item.querySelector('.slide-trigger-image');
-  const slideContainer = item.querySelector('.slide-container');
-  
-  // Activamos la desaparición de la imagen al pasar el ratón
-  item.addEventListener('mouseenter', () => {
-    slideTriggerImage.style.opacity = 0; // Hacemos que la imagen desaparezca
-  });
-  
-  // Restauramos la imagen cuando el ratón deja de estar encima
-  item.addEventListener('mouseleave', () => {
-    slideTriggerImage.style.opacity = 1; // Restauramos la visibilidad
-  });
+// Seleccionamos la imagen que debe desaparecer
+const slideTriggerImage = document.querySelector('.slide-trigger-image');
+const content = document.querySelector('.content');
+
+// Activamos la desaparición de la imagen al pasar el ratón
+content.addEventListener('mouseenter', () => {
+  slideTriggerImage.style.opacity = 0; // Hacemos que la imagen desaparezca
+});
+
+// Restauramos la imagen cuando el ratón deja de estar encima
+content.addEventListener('mouseleave', () => {
+  slideTriggerImage.style.opacity = 1; // Restauramos la visibilidad
 });
