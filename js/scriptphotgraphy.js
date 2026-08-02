ca42f05d947501e770da40f1e13abf138c1f3b32
Unai Benavente <161081469+benas20@users.noreply.github.com>
2026-08-02 17:53:50 +0200

    Update scriptphotgraphy.js

--- js/scriptphotgraphy.js
+++ js/scriptphotgraphy.js
@@ -1,79 +1,25 @@
-import * as THREE from "three";
-import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
-
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
 
-camera.position.set(0, 1.5, 6);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById("canvas"),
-    antialias: true,
-    alpha: true
-});
+camera.position.z = 5;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+function animate( time ) {
 
-// Luz ambiental
-const ambient = new THREE.AmbientLight(0xffffff, 2);
-scene.add(ambient);
+	cube.rotation.x = time / 2000;
+	cube.rotation.y = time / 1000;
 
-// Luz principal
-const directional = new THREE.DirectionalLight(0xffffff, 3);
-directional.position.set(5, 10, 7);
-scene.add(directional);
+	renderer.render( scene, camera );
 
-// Libro
-let book;
-const loader = new FBXLoader();
-loader.load(
-    
-    "livre.fbx",
-    function(fbx){
-        
-        book = fbx;
-        // Ajusta la escala según tu modelo
-        book.scale.set(0.01,0.01,0.01);
-        // Centrar el modelo
-        const box = new THREE.Box3().setFromObject(book);
-        const center = box.getCenter(new THREE.Vector3());
-        book.position.sub(center);
-        scene.add(book);
-    },
-    
-    undefined,
-    function(error){
-        console.error(error);
-    }
-);
-
-
-// Animación
-function animate(){
-
-    requestAnimationFrame(animate);
-    if(book){
-        book.rotation.y += 0.01;
-    }
-    renderer.render(scene,camera);
 }
-
-animate();
-
-// Responsive
-window.addEventListener("resize",()=>{
-
-    camera.aspect = window.innerWidth/window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth,window.innerHeight);
-
-});
